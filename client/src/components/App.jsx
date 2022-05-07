04a8c6067884109744aa80d57e32098a49fc721d
Joel Hench <joelhench@Olivias-MacBook-Pro-3.local>
2022-05-06 17:36:24 -0700

    skeleton

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SignUp from './signUp.js';
 import { AuthProvider } from '../contexts/AuthContext.js';
+import HomePage from './views/homepage/HomePage.jsx';
+import JudgeView from './views/judgeview/JudgeView.jsx';
+import PlayerView from './views/playerview/PlayerView.jsx';
+import Lobby from './views/lobby/Lobby.jsx';
 
 function App() {
+  const [pageview, setPageview] = useState('HomePage');
+
   return (
-    <AuthProvider>
-      <div>
-        <SignUp />
-      </div>
-    </AuthProvider>
+    <>
+      {pageview === 'HomePage' ? <HomePage/> : null}
+      {pageview === 'JudgeView' ? <JudgeView/> : null}
+      {pageview === 'PlayerView' ? <PlayerView/> : null}
+      {pageview === 'Lobby' ? <Lobby/> : null}
+      {/* <AuthProvider>
+        <div>
+          <SignUp />
+        </div>
+      </AuthProvider> */}
+    </>
   )
 }
 
