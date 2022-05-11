22c51f20d0bead1d1c3efd26150d53f249747559
James <qjameso@berkeley.edu>
2022-05-11 09:28:10 -0700

    style: style selected tab

--- client/src/components/views/navbar/Navbar.jsx
+++ client/src/components/views/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = ({ handleViewClick }) => {
+  const [selected, isSelected] = useState("HomePage");
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -10,9 +12,18 @@ const Navbar = ({ handleViewClick }) => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <a className="nav-link" onClick={() => handleViewClick("HomePage")}>Home</a>
-            <a className="nav-link" onClick={() => handleViewClick("Lobby")}>Lobby</a>
-            <a className="nav-link" onClick={() => handleViewClick("CustomDeck")}>Custom Decks</a>
+            <a className={selected === "HomePage" ? "nav-link active" : "nav-link"} onClick={() => {
+              isSelected("HomePage");
+              handleViewClick("HomePage");
+            }}>Home</a>
+            <a className={selected === "Lobby" ? "nav-link active" : "nav-link"} onClick={() => {
+              isSelected("Lobby");
+              handleViewClick("Lobby");
+            }}>Lobby</a>
+            <a className={selected === "CustomDeck" ? "nav-link active" : "nav-link"} onClick={() => {
+              isSelected("CustomDeck");
+              handleViewClick("CustomDeck");
+            }}>Custom Decks</a>
           </div>
         </div>
       </div>
