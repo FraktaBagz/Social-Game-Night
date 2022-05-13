2cd3894072178cc7b65ee7c9c6dc3377ada6a916
kimberlysiu1@gmail.com <kimberlysiu1@gmail.com>
2022-05-12 21:51:46 -0700

    Customize cards view styling

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -162,24 +162,26 @@ export default function App() {
   const { getUser, getDeck, getDecks } = useGame();
   const [pageView, setPageView] = useState('SignIn');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [pageView, setPageView] = useState('Custom');
+  // const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [gameState, setGameState] = useState({});
   const [defaultDeck, setDefaultDeck] = useState(customDecksSample.skip);
   const [customDecks, setCustomDecks] = useState(customDecksSample);
   const [selectedCustomDeck, setSelectedCustomDeck] = useState({
-    dummy: {
+    title: {
       // questions: [
       greenCard: [
         {
-          label: 'some prompt',
-          extra: '(obsessive, consuming, captivating) ',
-          sets: 'default green',
+          label: 'adjective',
+          extra: '(synonyms) ',
+          sets: 'deck title',
         }],
       // answers: [
       redCard: [
         {
-          label: 'Addictive',
-          extra: '(obsessive, consuming, captivating) ',
-          sets: 'default red',
+          label: 'noun',
+          extra: 'sentence',
+          sets: 'deck title',
         }]
     },
   });
